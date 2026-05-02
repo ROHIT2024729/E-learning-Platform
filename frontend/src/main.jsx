46c381d15ae00dba9d4f7ab43676b456e0c1770d
ROHIT2024729 <rohitsingh7905314@gmail.com>
2026-05-03 00:13:34 +0530

    vite_urladded

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 import { AdminContextProvider } from './context/AdminContext.jsx';
-export const server = 'http://localhost:3000';
+export const server = import.meta.env.VITE_API_URL;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
